b1ab43395c7cd9b6ed068b4fbcbe2a04926ed49b
vera bessonova <v2112365@gmail.com>
2025-02-28 21:03:00 +0300

    add formula to CreditPaymentService & change file main

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,19 +1,24 @@
 public class CreditPaymentService {
 
-    public int Calculate(int credit, double percent, int month) {
+    public double Calculate(double credit, double percent, double month) {
 
 
 
-        double percentMonth = percent / month / 100;
+        double percentMonth = percent / month / 100; // расчет процента в месяц
 
         double k = 1 + percentMonth; //переменная (процент в месяц+1) для возведения в степень.
         double y = Math.pow(k, month); //вызов метода возведения в степень
+        double coefficientAnnuity = (percentMonth * y) / (y - 1); //вычисление коэффицента аннуитета
 
-        double coefficientAnnuity = (percentMonth * y) / (y - 1);
-        double payment = credit * coefficientAnnuity;
-        int myPayment = (int) payment;
+       // double coefficientAnnuity = (percentMonth * (Math.pow(1 + percentMonth, month))) / (Math.pow(1 + percentMonth, month) - 1); // та же формула что и выше
+        double payment = credit * coefficientAnnuity; // вычисление ежемесячног платежа
 
-        return myPayment;
+        //double x = Math.pow((1 + percent / 12), month);
+        //double payment = credit * (percent / 12 * (x / (x - 1))); // другая формула, получается еще хуже, вообще аномальные цифры
+
+        // int myPayment = (int) payment;
+
+        return payment;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         CreditPaymentService service = new CreditPaymentService();
-int credit = 1_000_000;
+double credit = 1_000_000;
 double percent = 9.99;
-int month = 12;
+double month;
 
-int myPayment = service.Calculate(credit, percent, month);
-
+int myPayment = (int) service.Calculate(credit, percent, 12);
 System.out.println("Ежемесячный платеж: " + myPayment);
+
+int myPayment1 = (int) service.Calculate(credit, percent, 24);
+System.out.println("Ежемесячный платеж: " + myPayment1);
+
+int myPayment2 = (int) service.Calculate(credit, percent, 36);
+System.out.println("Ежемесячный платеж: " + myPayment2);
 
     }
 }
