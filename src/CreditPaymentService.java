b740a8abc9234b855fe4caa1061f45cb63f81a24
vera bessonova <v2112365@gmail.com>
2025-03-01 10:59:30 +0300

    changed formula percentMonth

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,23 +2,16 @@ public class CreditPaymentService {
 
     public double Calculate(double credit, double percent, double month) {
 
+      double percentMonth = percent / 12 / 100; // расчет процента в месяц
+      double k = 1 + percentMonth; //переменная (процент в месяц+1) для возведения в степень.
+      double y = Math.pow(k, month); //вызов метода возведения в степень
+      double coefficientAnnuity = (percentMonth * y) / (y - 1); //вычисление коэффицента аннуитета
 
+      //  double coefficientAnnuity = (percentMonth * (Math.pow(1 + percentMonth, month))) /
+      //   (Math.pow(1 + percentMonth, month) - 1); // та же формула что и выше
 
-        double percentMonth = percent / month / 100; // расчет процента в месяц
+      double payment = credit * coefficientAnnuity; // вычисление ежемесячного платежа
 
-        double k = 1 + percentMonth; //переменная (процент в месяц+1) для возведения в степень.
-        double y = Math.pow(k, month); //вызов метода возведения в степень
-        double coefficientAnnuity = (percentMonth * y) / (y - 1); //вычисление коэффицента аннуитета
-
-       // double coefficientAnnuity = (percentMonth * (Math.pow(1 + percentMonth, month))) / (Math.pow(1 + percentMonth, month) - 1); // та же формула что и выше
-        double payment = credit * coefficientAnnuity; // вычисление ежемесячног платежа
-
-        //double x = Math.pow((1 + percent / 12), month);
-        //double payment = credit * (percent / 12 * (x / (x - 1))); // другая формула, получается еще хуже, вообще аномальные цифры
-
-        // int myPayment = (int) payment;
-
-        return payment;
+    return payment;
     }
-
 }
